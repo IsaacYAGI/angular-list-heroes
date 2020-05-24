375a51c95ccbef5d39b944b4d17c87545f4be7a2
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-24 14:37:21 -0500

    Se leen los heroes del servicio

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HeroesService } from 'src/app/services/heroes.service';
+import { HeroesService, Heroe } from 'src/app/services/heroes.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,9 +8,13 @@ import { HeroesService } from 'src/app/services/heroes.service';
 })
 export class HeroesComponent implements OnInit {
 
+  heroes: Heroe[] = [];
+
   constructor(private _heroesService: HeroesService) { }
 
   ngOnInit(): void {
+    this.heroes = this._heroesService.getHeroes();
+    console.log(this.heroes)
   }
 
 }
