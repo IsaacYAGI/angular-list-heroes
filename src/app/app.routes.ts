b989c31310683b07e971e0be15a52389f5db3829
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-24 13:19:45 -0500

    Agrega el archivo de rutas y la redireccion al home

---
+++ src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { RouterModule, Route, Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { HomeComponent } from './components/home/home.component';
+
+const APP_ROUTES: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: '**', pathMatch: 'full', redirectTo: 'home'},
+    
+];
+
+export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES);
