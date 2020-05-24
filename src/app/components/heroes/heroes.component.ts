d32127b6fa5654c5dac003d60e2ee1941e68fc91
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-24 15:05:41 -0500

    Agrega la navegacion a pagina de detalle de heroe

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HeroesService, Heroe } from 'src/app/services/heroes.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-heroes',
@@ -10,11 +11,19 @@ export class HeroesComponent implements OnInit {
 
   heroes: Heroe[] = [];
 
-  constructor(private _heroesService: HeroesService) { }
+  constructor(
+    private _heroesService: HeroesService,
+    private router: Router
+    
+    ) { }
 
   ngOnInit(): void {
     this.heroes = this._heroesService.getHeroes();
-    console.log(this.heroes)
+    //console.log(this.heroes)
+  }
+
+  verHeroe(i: number): void{
+    this.router.navigate( ['/heroe',i] );
   }
 
 }
