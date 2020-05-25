b16646dc22133e1190b26cb790d56dc75ccf7df4
Isaac Yriarte <isaacyagi@gmail.com>
2020-05-24 19:39:11 -0500

    Ajuste para evitar que al darle enter se recargue la pagina al buscar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 //Rutas
 
@@ -32,7 +33,8 @@ import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.
   ],
   imports: [
     BrowserModule,
-    APP_ROUTING
+    APP_ROUTING,
+    FormsModule
   ],
   providers: [
     HeroesService
